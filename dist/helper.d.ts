b572a327caf5955bf4da0bd38b6c9d6ff3329d70
Max Wan <mwan@moneytree.jp>
2020-11-11 17:59:35 +0900

    Temporary latest build

--- dist/helper.d.ts
+++ dist/helper.d.ts
@@ -3,3 +3,4 @@ export declare function constructScopes(scopes?: Scopes): string | undefined;
 export declare function getIsTabValue(isNewTab?: boolean): '' | '_self';
 export declare function mergeConfigs(initValues: InitOptions, newValues: ConfigsOptions, ignoreKeys?: string[]): ConfigsOptions;
 export declare function generateConfigs(configs?: ConfigsOptions): string;
+export declare function generateCodeChallenge(): string;
